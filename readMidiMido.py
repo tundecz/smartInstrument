5c3014f55bc6c7e303256ee5b09b676327cbf2fc
tundecz <tunde.czoguly98@gmail.com>
2020-03-22 13:58:16 +0200

    Added helper and commets

--- readMidiMido.py
+++ readMidiMido.py
@@ -5,13 +5,12 @@ import time
 import constants
 import logging
 import motorVibration as motor
-import helper
+from helper import Helper
 
 class ReadMidi:
     
     def __init__(self, port):
         print("Constructor")
-        # self._midiIn, self._midiPort = midiutil.open_midiinput(port)
         self._midiIn = mido.open_input(port)
         print(self._midiIn)
         self._motor = motor.VibrationMotor()
@@ -25,9 +24,7 @@ class ReadMidi:
     def midiIn(self, midiIn):
         self._midiIn = midiIn
 
-
     def list_input_ports(self):
-        # midiutil.list_input_ports()
         mido.get_input_names()
 
     def _close_input_port(self):
@@ -35,6 +32,7 @@ class ReadMidi:
         print("Midi port closed")
     
 
+    # parsing the array and returning the values seperately
     def _get_note_and_velocity(self, message):
         try:
             note = message[1]
@@ -43,10 +41,13 @@ class ReadMidi:
         except:
             print("Something went wrong with byte parsing")
 
+    # returns true if the array is a pressed note 
+    # if no note was pressed, the array looks like this => [248]
     def _is_pressed_note(self, bytes_array):
         return True if bytes_array.__len__() > 1 else False
-
     
+
+    # get the midi messages from the piano, convert them into bytes and send information to Vibration class
     def _get_midi_messages(self):
         try:
             timer = time.time()
@@ -55,7 +56,7 @@ class ReadMidi:
                     bytes_array = message.bytes()
                     if(self._is_pressed_note(bytes_array)):
                         note, velocity = self._get_note_and_velocity(bytes_array)
-                        ansi_note = self._toNote(note)
+                        ansi_note = Helper.number_to_note(note)
                         print("Note %s pressed with %d velocity" %(ansi_note, velocity))
                         # self._motor.vibrate(1,note)
                     else:
@@ -67,9 +68,7 @@ class ReadMidi:
         finally:
             self._close_input_port()
     
-    def _toNote(self, number):
-        return constants.NOTES[number % 12]
-    
+    # entry point from main class
     def run(self):
         self.list_input_ports()
         self._get_midi_messages()
