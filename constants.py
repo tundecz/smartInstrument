99b97fac91a4523865711f2f8dce2790a3d4dfca
tundecz <tunde.czoguly98@gmail.com>
2020-04-09 21:07:41 +0300

    Added constants

--- constants.py
+++ constants.py
@@ -3,5 +3,7 @@ NOTES = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a', 'b']
 GPIO_PIN_14 = 14
 NOTE_OFF_VALUE = 0
 DEFAULT_FREQUENNCY = 100
+MIDDLE_C = 64
+MIDDLE_C_VALUE = 0.5
 
 #need more gpio pins when working with more motors
