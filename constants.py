b02f2139e8a13fd4abef252b209a41fe5e5edc2a
tundecz <tunde.czoguly98@gmail.com>
2020-04-11 15:47:07 +0300

    refaxtored constants

--- constants.py
+++ constants.py
@@ -1,9 +1,12 @@
 PORT_NAME = 'CH345:CH345 MIDI 1 20:0'
 NOTES = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a', 'b']
-GPIO_PIN_14 = 14
+
+# gpio numbers for the the motors
+GPIO_PIN_14_BASS_CLEF = 14
+GPIO_PIN_15_TREBLE_CLEF = 15
+
+# vibration motor values and frequencies
 NOTE_OFF_VALUE = 0
 DEFAULT_FREQUENNCY = 100
 MIDDLE_A = 69
-MIDDLE_A_VALUE = 0.5
-
-#need more gpio pins when working with more motors
+MIDDLE_A_VALUE = 0.5    
