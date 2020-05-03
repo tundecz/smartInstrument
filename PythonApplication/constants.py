e335c635fe28eb46151971e2e6c363051014fc69
tundecz <tunde.czoguly98@gmail.com>
2020-05-03 16:11:48 +0300

    Changes host to raspberrypi.local

--- PythonApplication/constants.py
+++ PythonApplication/constants.py
@@ -2,7 +2,7 @@ PORT_NAME = 'CH345:CH345 MIDI 1 20:0'
 NOTES = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a', 'b']
 
 # client-server stuff
-HOST = '192.168.1.11'
+HOST = 'raspberrypi.local'
 PORT = 65432
 
 # vibration motor values and frequencies
