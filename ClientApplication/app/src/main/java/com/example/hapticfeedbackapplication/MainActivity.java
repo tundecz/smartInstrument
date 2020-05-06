e90d3dab16075e92590bd9b439d8a2a83c1dbbaa
tundecz <tunde.czoguly98@gmail.com>
2020-05-06 20:01:52 +0300

    Improved Android UI

--- ClientApplication/app/src/main/java/com/example/hapticfeedbackapplication/MainActivity.java
+++ ClientApplication/app/src/main/java/com/example/hapticfeedbackapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.net.Socket;
 
@@ -13,23 +14,54 @@ public class MainActivity extends AppCompatActivity {
     String SERVER_IP;
     Integer SERVER_PORT;
     Switch onOffSwitch;
-    SeekBar bassSeekBar;
+    Switch bassOnOffSwitch;
+    Switch trebleOnOffSwitch;
+    Switch highOnOffSwitch;
     SeekBar seekBar;
+    int seekBarProgress = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onOffSwitch = findViewById(R.id.on_off_switch);
-        bassSeekBar = findViewById(R.id.bass_seek_bar);
-        seekBar = findViewById(R.id.seek_bar);
+        initializeVariables();
+        setDefaultValues();
 
-        onOffSwitch.setChecked(true); // wa want to be on when we start the application
+       // wa want to be on when we start the application
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),"seekbar progress: " + progress, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText(getApplicationContext(), "seekbar touch started!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText(getApplicationContext(), "sekkabr touched stopped!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 //        new Thread(new ClientThread()).start();
     }
 
-    public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser){
+    private void initializeVariables(){
+        onOffSwitch = findViewById(R.id.on_off_switch);
+        bassOnOffSwitch= findViewById(R.id.bass_on_off_switch);
+        trebleOnOffSwitch = findViewById(R.id.treble_on_off_switch);
+        highOnOffSwitch = findViewById(R.id.high_on_off_switch);
+        seekBar = findViewById(R.id.seek_bar);
+    }
 
+    private void setDefaultValues(){
+        onOffSwitch.setChecked(true);
+        bassOnOffSwitch.setChecked(true);
+        trebleOnOffSwitch.setChecked(true);
+        highOnOffSwitch.setChecked(true);
+        seekBar.setProgress(seekBarProgress);
     }
 }
