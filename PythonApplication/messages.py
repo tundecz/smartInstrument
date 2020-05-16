fb4b2974e4d60299c1a3c8f66da0d73fcbf1a4ab
tundecz <tunde.czoguly98@gmail.com>
2020-05-16 19:04:46 +0300

    Implemented dictionary  for messages to easier vibration modification from the client

--- PythonApplication/messages.py
+++ PythonApplication/messages.py
@@ -1,5 +1,5 @@
 import enum
-import re
+# import re
 
 class Message(enum.Enum):
     ON = 1
@@ -9,6 +9,7 @@ class Message(enum.Enum):
     HIGH = 5
     SWITCH = 6
     PROGRESS = 7
+    RESET = 8
     # RESET = 
 
 # enum mapping
@@ -18,5 +19,19 @@ MESSAGES = {
     Message.BASS: "bass",
     Message.TREBLE: "treble",
     Message.HIGH: "high",
-    Message.PROGRESS: "progress"
+    Message.PROGRESS: "progress",
+    Message.SWITCH: "switch",
+    Message.RESET: "reset"
+}
+
+# this dictionary is necessary to pass information about what action was taken on the phone
+# at the beggining all the value are true (are set to a default value)
+# when the bass switch is switched to off, the Message.BASS will be false, so the user'll not feel the bass notes
+# because the information i passed b reference, don't need to 
+DEFAULT_VALUES = {
+    Message.SWITCH: True,
+    Message.BASS: True,
+    Message.TREBLE: True,
+    Message.HIGH: True,
+    Message.PROGRESS: 0.5
 }
--- PythonApplication/readMidiMido.py
+++ PythonApplication/readMidiMido.py
@@ -7,7 +7,7 @@ from motorVibration import VibrationMotor
 from helper import Helper
 from constants import HOST, PORT
 import bridge
-from messages import MESSAGES, Message
+from messages import MESSAGES, Message, DEFAULT_VALUES
 
 
 class ReadMidi():
@@ -74,27 +74,42 @@ class ReadMidi():
     # we need to check if the event is set, then dequeue and check what command was passed
     # note_type = bass/treble/high/value
     # value = on/off/integer
+    # default all the boolean params will be true
+    # for the progress the parameter will be a value
     def _processEvent(self):
         message = bridge.dequeue_message()
         note_type, value = Helper.parse_dequeued_message(message)
         print("Message received in the readmidi class {}".format(message))
-        if note_type is MESSAGES[Message.BASS]:
-            if value is MESSAGES[Message.ON]:
-                pass
-            elif value is MESSAGES[Message.OFF]:
-                pass
+        if note_type is MESSAGES[Message.SWITCH]:
+           self._setValues(value, Message.SWITCH)
+        elif note_type is MESSAGES[Message.BASS]:
+            self._setValues(value, Message.BASS)
         elif note_type is MESSAGES[Message.TREBLE]:
-            if value is MESSAGES[Message.ON]:
-                pass
-            elif value is MESSAGES[Message.OFF]:
-                pass
+            self._setValues(value, Message.TREBLE)
         elif note_type is MESSAGES[Message.HIGH]:
-            if value is MESSAGES[Message.ON]:
-                pass
-            elif value is MESSAGES[Message.OFF]:
-                pass
+            self._setValues(value, Message.HIGH)
         elif note_type is MESSAGES[Message.PROGRESS]:
             progress_value = int(value)
+            DEFAULT_VALUES[Message.PROGRESS] = progress_value
+        elif note_type is MESSAGES[Message.RESET]:
+            self._resetToDefaultValues()
+            
+
+    # set the values for DEFAULT_VALUE dictionary based on message got from Android application
+    def _setValues(self, value, switch_type):
+        if value is MESSAGES[Message.ON]:
+            DEFAULT_VALUES[switch_type] = True
+            print(str(DEFAULT_VALUES[switch_type]) + "changed to true")
+        elif value is MESSAGES[Message.OFF]:
+            DEFAULT_VALUES[switch_type] = False
+            print(str(DEFAULT_VALUES[switch_type]) + "changed to false")
+
+    def _resetToDefaultValues(self):
+        DEFAULT_VALUES[Message.BASS] = True
+        DEFAULT_VALUES[Message.TREBLE] = True
+        DEFAULT_VALUES[Message.HIGH] = True
+        DEFAULT_VALUES[Message.SWITCH] = True
+        DEFAULT_VALUES[Message.PROGRESS] = 0,5
 
 
     # get the midi messages from the piano, convert them into bytes and send information to Vibration class
@@ -121,3 +136,4 @@ class ReadMidi():
     def run(self):
         self.list_input_ports()
         self._get_midi_messages()
+    
