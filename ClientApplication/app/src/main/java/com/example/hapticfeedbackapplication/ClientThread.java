f1db3e56ad3bcf04a6a2a911d40c0d6d7e3f4de9
tundecz <tunde.czoguly98@gmail.com>
2020-05-29 22:24:13 +0300

    Implementation on recieving messages from server

--- ClientApplication/app/src/main/java/com/example/hapticfeedbackapplication/ClientThread.java
+++ ClientApplication/app/src/main/java/com/example/hapticfeedbackapplication/ClientThread.java
@@ -1,30 +1,34 @@
 package com.example.hapticfeedbackapplication;
 
 import android.util.Log;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.BufferedWriter;
-import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ClientThread implements Runnable {
 
     private Socket socket;
-    private BufferedReader bufferedReader;
+//    private BufferedReader bufferedReader;
+    private DataInputStream in;
     private static final String SERVER_IP = "localhost";
+    private InputStream input;
+//    public static
 
     @Override
     public void run() {
         try {
 //         InetAddress servAddrs = InetAddress.getByName(SERVER_IP);
-         socket = new Socket("192.168.1.12", 65432);
+         socket = new Socket("192.168.1.8", 65432);
          Log.d("socket","Socket binded");
+         in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+         Log.d("socket","Socket binded");
+         new Thread(new MessageThread()).start();
         } catch (UnknownHostException e) {
             System.out.println("Server not found");
         } catch (IOException e) {
@@ -47,4 +51,24 @@ public class ClientThread implements Runnable {
             }
         }).start();
     }
+
+    class MessageThread implements Runnable{
+
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    Log.d("socket","test");
+                    byte[] message = new byte[1024];
+                    in.read(message);
+                    String m = new String(message, "UTF-8");
+                    Log.d("socket",m);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
+
